4417ec9d9b8e96da11c0aacb5a6a0b8abacfba3d
deadprogram <ron@hybridgroup.com>
2014-08-27 18:30:47 -0700

    Generated skeleton

---
+++ test/specs/adaptor.spec.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var M2x = source("adaptor");
+
+describe("Cylon.Adaptors.M2x", function() {
+  var adaptor = new M2x();
+
+  it("needs tests");
+});
---
+++ test/specs/driver.spec.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var M2x = source("driver");
+
+describe("Cylon.Drivers.M2x", function() {
+  var driver = new M2x({
+    device: { connection: 'connect' }
+  });
+
+  it("needs tests");
+});
