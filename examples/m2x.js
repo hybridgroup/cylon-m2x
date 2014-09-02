2975dfde6afabb6fd781062c7e0124bfb87122d2
Andrew Stewart <andrew@stwrt.ca>
2014-09-02 09:59:29 -0700

    Correctly correctly with M2X

--- examples/m2x.js
+++ examples/m2x.js
@@ -1,17 +1,24 @@
 var Cylon = require('cylon');
 
 Cylon.robot({
-  connection: { name: 'm2x', adaptor: 'm2x', apiKey: 'key', feedId: 'feedId' },
-  device: {name: 'm2x', driver: 'm2x'},
+  connection: {
+    name: 'm2x',
+    adaptor: 'm2x',
+    apiKey: '???',
+    feedId: '???'
+  },
+
+  device: { name: 'm2x', driver: 'm2x' },
 
   work: function(my) {
-    var count = 0;
+    var count = 1;
 
-    every(100, function() {
-      my.m2x.push('topic1', { count: count++ });
+    every(2000, function() {
+      console.log("Setting test-topic to " + count);
+      my.m2x.push('test-topic', { value: count++ });
     });
 
-    my.m2x.subscribe('topic1', function(data) {
+    my.m2x.subscribe('test-topic', function(data) {
       console.log(data);
     });
   }
