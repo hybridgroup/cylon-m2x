bdfceb6cc73199f48a74d5573366a1d1ddf10411
Andrew Stewart <andrew@stwrt.ca>
2014-11-04 14:28:40 -0800

    Update module registration for latest Cylon changes

--- test/specs/cylon-m2x.spec.js
+++ test/specs/cylon-m2x.spec.js
@@ -6,9 +6,15 @@ var Adaptor = source('adaptor'),
     Driver = source('driver');
 
 describe("Cylon.M2x", function() {
-  describe("#register", function() {
-    it("should be a function", function() {
-      expect(module.register).to.be.a('function');
+  describe("#adaptors", function() {
+    it('is an array of supplied adaptors', function() {
+      expect(module.adaptors).to.be.eql(['m2x']);
+    });
+  });
+
+  describe("#drivers", function() {
+    it('is an array of supplied drivers', function() {
+      expect(module.drivers).to.be.eql(['m2x']);
     });
   });
 
