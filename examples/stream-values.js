ec4f0499f146e8cc1d67d26b7070aba3e8d6dda9
edgarsilva <edgar.osc@gmail.com>
2015-01-07 16:07:30 -0600

    Update readme for m2x V2.0 and add new example.

---
+++ examples/stream-values.js
@@ -0,0 +1,21 @@
+/*jslint node: true */
+"use strict";
+
+var Cylon = require("cylon");
+
+Cylon.robot({
+  connections: {
+    m2x: { adaptor: "m2x", masterKey: "<masterKey>" }
+  },
+
+  devices: {
+    m2x: { driver: "m2x" }
+  },
+
+  work: function(my) {
+    my.m2x.streamValues("<deviceId>", "temp", {}, function(err, data) {
+      console.log("Err: ", err);
+      console.log("Values: ", data);
+    });
+  }
+}).start();
